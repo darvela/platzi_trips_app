a4656ee729a4d769bca00bb5eff02f2b28d4fe58
darvela@gmail.com <Ju4nD1ego>
2020-03-17 22:40:57 -0500

    Se centra los child del stack

--- lib/main.dart
+++ lib/main.dart
@@ -7,46 +7,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hola Mundo Hermoso"),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
         ),
-        body: Center(
-          child: Stack(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hola Mundo Hermoso"),
+          ),
+          body: Center(
+              child: Stack(
+            alignment: Alignment.center,
             children: <Widget>[
               const Image(
                 //image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                image: NetworkImage('https://raw.githubusercontent.com/darvela/assets/master/pesca.jpeg'),
+                image: NetworkImage(
+                    'https://raw.githubusercontent.com/darvela/assets/master/pesca.jpeg'),
               ),
               Container(
-                width: 410,
-                height: 250,
+                width: 400,
+                height: 100,
                 color: Color.fromRGBO(255, 255, 255, 0.5),
               ),
               Text(
-                'My own photo?',
+                'Segundo reto termiado',
                 textAlign: TextAlign.center,
                 overflow: TextOverflow.ellipsis,
                 style: TextStyle(fontWeight: FontWeight.bold),
               )
             ],
-          )
-        ),
-      )//MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+          )),
+        ) //MyHomePage(title: 'Flutter Demo Home Page'),
+        );
   }
 }
 
